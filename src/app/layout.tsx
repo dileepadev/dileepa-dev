64659c4e3f04a438f45269d34dc81c74c50e89ef
dileepadev <251038784+dileepadev@users.noreply.github.com>
2026-01-15 23:35:12 +0530

    fix(layout): Correct ThemeProvider import casing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme/themeProvider";
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { ToastNotification } from "@/components/ToastNotification";
 import metadataJSON from "@/data/metaData";
 import { Analytics } from "@vercel/analytics/next";
